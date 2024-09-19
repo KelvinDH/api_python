75898c0728ffedf02d9fbfd98fadd1dd4c150694
Kelvin <100075444+KelvinDH@users.noreply.github.com>
2024-09-19 15:35:20 -0300

    Add files via upload

---
+++ data_pedidos.py
@@ -0,0 +1,249 @@
+# data.py
+pedidos = [
+     {
+    "id_pedido": 4,
+    "id_usuario": 1,
+    "id_cliente": 2,
+    "dt_pedido": "2024-09-02T00:00:00.000Z",
+    "vl_total": 35,
+    "nome": "99 Coders",
+    "cidade": "São Paulo",
+    "usuario": "Heber Stein Mazutti"
+  },
+  {
+    "id_pedido": 3,
+    "id_usuario": 1,
+    "id_cliente": 2,
+    "dt_pedido": "2024-09-02T00:00:00.000Z",
+    "vl_total": 35,
+    "nome": "99 Coders",
+    "cidade": "São Paulo",
+    "usuario": "Heber Stein Mazutti"
+  },
+  {
+    "id_pedido": 2,
+    "id_usuario": 2,
+    "id_cliente": 5,
+    "dt_pedido": "2024-09-05T00:00:00.000Z",
+    "vl_total": 40,
+    "nome": "Amazon",
+    "cidade": "São Paulo",
+    "usuario": "João Antunes dos Santos"
+  },
+
+  {
+    "id_pedido": 2,
+    "id_usuario": 2,
+    "id_cliente": 5,
+    "dt_pedido": "2024-09-05T00:00:00.000Z",
+    "vl_total": 40,
+    "nome": "Amazon",
+    "cidade": "São Paulo",
+    "usuario": "João Antunes dos Santos"
+  },
+  
+  {
+    "id_pedido": 3,
+    "id_usuario": 1,
+    "id_cliente": 2,
+    "dt_pedido": "2024-09-02T00:00:00.000Z",
+    "vl_total": 35,
+    "nome": "99 Coders",
+    "cidade": "São Paulo",
+    "usuario": "Heber Stein Mazutti"
+  },
+
+    {
+    "id_pedido": 7,
+    "id_usuario": 7,
+    "id_cliente": 12,
+    "dt_pedido": "2024-09-03T00:00:00.000Z",
+    "vl_total": 220,
+    "nome": "TechBrasil Sistemas de Segurança",
+    "cidade": "Bauru",
+    "usuario": "Pedro Victor Manhani Acosta"
+  },
+  {
+    "id_pedido": 4,
+    "id_usuario": 1,
+    "id_cliente": 2,
+    "dt_pedido": "2024-09-02T00:00:00.000Z",
+    "vl_total": 35,
+    "nome": "99 Coders",
+    "cidade": "São Paulo",
+    "usuario": "Heber Stein Mazutti"
+  },
+  {
+    "id_pedido": 3,
+    "id_usuario": 1,
+    "id_cliente": 2,
+    "dt_pedido": "2024-09-02T00:00:00.000Z",
+    "vl_total": 35,
+    "nome": "99 Coders",
+    "cidade": "São Paulo",
+    "usuario": "Heber Stein Mazutti"
+  },
+  {
+    "id_pedido": 2,
+    "id_usuario": 2,
+    "id_cliente": 5,
+    "dt_pedido": "2024-09-05T00:00:00.000Z",
+    "vl_total": 40,
+    "nome": "Amazon",
+    "cidade": "São Paulo",
+    "usuario": "João Antunes dos Santos"
+  },
+
+  {
+    "id_pedido": 2,
+    "id_usuario": 2,
+    "id_cliente": 5,
+    "dt_pedido": "2024-09-05T00:00:00.000Z",
+    "vl_total": 40,
+    "nome": "Amazon",
+    "cidade": "São Paulo",
+    "usuario": "João Antunes dos Santos"
+  },
+  
+  {
+    "id_pedido": 3,
+    "id_usuario": 1,
+    "id_cliente": 2,
+    "dt_pedido": "2024-09-02T00:00:00.000Z",
+    "vl_total": 35,
+    "nome": "99 Coders",
+    "cidade": "São Paulo",
+    "usuario": "Heber Stein Mazutti"
+  },
+
+  {
+    "id_pedido": 7,
+    "id_usuario": 7,
+    "id_cliente": 12,
+    "dt_pedido": "2024-09-03T00:00:00.000Z",
+    "vl_total": 220,
+    "nome": "TechBrasil Sistemas de Segurança",
+    "cidade": "Bauru",
+    "usuario": "Pedro Victor Manhani Acosta"
+  },
+  {
+    "id_pedido": 4,
+    "id_usuario": 1,
+    "id_cliente": 2,
+    "dt_pedido": "2024-09-02T00:00:00.000Z",
+    "vl_total": 35,
+    "nome": "99 Coders",
+    "cidade": "São Paulo",
+    "usuario": "Heber Stein Mazutti"
+  },
+  {
+    "id_pedido": 3,
+    "id_usuario": 1,
+    "id_cliente": 2,
+    "dt_pedido": "2024-09-02T00:00:00.000Z",
+    "vl_total": 35,
+    "nome": "99 Coders",
+    "cidade": "São Paulo",
+    "usuario": "Heber Stein Mazutti"
+  },
+  {
+    "id_pedido": 2,
+    "id_usuario": 2,
+    "id_cliente": 5,
+    "dt_pedido": "2024-09-05T00:00:00.000Z",
+    "vl_total": 40,
+    "nome": "Amazon",
+    "cidade": "São Paulo",
+    "usuario": "João Antunes dos Santos"
+  },
+
+  {
+    "id_pedido": 2,
+    "id_usuario": 2,
+    "id_cliente": 5,
+    "dt_pedido": "2024-09-05T00:00:00.000Z",
+    "vl_total": 40,
+    "nome": "Amazon",
+    "cidade": "São Paulo",
+    "usuario": "João Antunes dos Santos"
+  },
+  
+  {
+    "id_pedido": 3,
+    "id_usuario": 1,
+    "id_cliente": 2,
+    "dt_pedido": "2024-09-02T00:00:00.000Z",
+    "vl_total": 35,
+    "nome": "99 Coders",
+    "cidade": "São Paulo",
+    "usuario": "Heber Stein Mazutti"
+  },
+
+  {
+    "id_pedido": 2,
+    "id_usuario": 2,
+    "id_cliente": 5,
+    "dt_pedido": "2024-09-05T00:00:00.000Z",
+    "vl_total": 40,
+    "nome": "Amazon",
+    "cidade": "São Paulo",
+    "usuario": "João Antunes dos Santos"
+  },
+
+
+    {
+    "id_pedido": 7,
+    "id_usuario": 7,
+    "id_cliente": 12,
+    "dt_pedido": "2024-09-03T00:00:00.000Z",
+    "vl_total": 220,
+    "nome": "TechBrasil Sistemas de Segurança",
+    "cidade": "Bauru",
+    "usuario": "Pedro Victor Manhani Acosta"
+  },
+  {
+    "id_pedido": 4,
+    "id_usuario": 1,
+    "id_cliente": 2,
+    "dt_pedido": "2024-09-02T00:00:00.000Z",
+    "vl_total": 35,
+    "nome": "99 Coders",
+    "cidade": "São Paulo",
+    "usuario": "Heber Stein Mazutti"
+  },
+
+  {
+    "id_pedido": 2,
+    "id_usuario": 2,
+    "id_cliente": 5,
+    "dt_pedido": "2024-09-05T00:00:00.000Z",
+    "vl_total": 40,
+    "nome": "Amazon",
+    "cidade": "São Paulo",
+    "usuario": "João Antunes dos Santos"
+  },
+
+  {
+    "id_pedido": 7,
+    "id_usuario": 7,
+    "id_cliente": 12,
+    "dt_pedido": "2024-09-03T00:00:00.000Z",
+    "vl_total": 220,
+    "nome": "TechBrasil Sistemas de Segurança",
+    "cidade": "Bauru",
+    "usuario": "Pedro Victor Manhani Acosta"
+  },
+
+  {
+    "id_pedido": 3,
+    "id_usuario": 1,
+    "id_cliente": 2,
+    "dt_pedido": "2024-09-02T00:00:00.000Z",
+    "vl_total": 35,
+    "nome": "99 Coders",
+    "cidade": "São Paulo",
+    "usuario": "Heber Stein Mazutti"
+  },
+
+]
+  
